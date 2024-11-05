a653ad6ca413e11eb31dda31a3b2737d0a3eecf9
Freddy Cuellar <facuellar@unal.edu.co>
2024-11-04 21:39:34 -0500

    add button to emotion :tada:

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,59 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return ChangeNotifierProvider(
+      create: (context) => AppState(),
+      child: MaterialApp(
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+        ),
+        home: HomePage(),
+      ),
+    );
+  }
+}
+
+class AppState extends ChangeNotifier {
+  var greeting = 'Hello World!';
+
+  void addExclamation() {
+    greeting += '!';
+    notifyListeners();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var appState = context.watch<AppState>();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DADM 2024'),
+        backgroundColor: theme.colorScheme.primary,
+        foregroundColor: theme.colorScheme.onPrimary,
+      ),
+      body: Center(
+        child: Text(appState.greeting),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          appState.addExclamation();
+        },
+        tooltip: "EMOTIOOOOOONNN",
+        child: Icon(Icons.emoji_emotions),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        color: theme.colorScheme.primary,
+        child: Container(
+          height: 50.0,
         ),
       ),
     );
